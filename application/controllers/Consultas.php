3294f15adc5db9f88c25e5e6f30fb835dffb671d
Diego Cayetano <dact_96@hotmail.com>
2019-03-17 18:00:18 -0500

    Actualizacion 17-03-19

--- application/controllers/Consultas.php
+++ application/controllers/Consultas.php
@@ -6,14 +6,12 @@ class Consultas extends CI_Controller{
 		parent::__construct();
 		$this->load->model("Usuarios_model","usuBD");
 		$this->load->model("EstadoVisita_model","estVisitaBD");
-		$this->load->model("Paciente_model","pacienteBD");
 	}
     
     public function index()
 	{
 		$data["usuarios"] = $this->usuBD->getAll();
 		$data["estadoVisitas"] = $this->estVisitaBD->getAll();
-		$data["pacientes"] = $this->pacienteBD->getAll();
 		$this->load->view('consultas', $data);
 	}
     
